0db0b1df8a50eb94cfb67c9c63650995ab2d040e
CandeClara <131951638+CandeClara@users.noreply.github.com>
2023-05-15 13:28:41 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,6 @@
+let Dirección_origen = "AR"
+let Dirección_destino = "GP"
+let MENSAJE = "" + Dirección_origen + Dirección_destino + "TREMENDO CULÓN"
+basic.forever(function () {
+    radio.setGroup(0)
+})
