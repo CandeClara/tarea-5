2cc7cf4247eb19ca9de3025322490eedd8512c43
CandeClara <131951638+CandeClara@users.noreply.github.com>
2023-05-18 09:09:17 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString(PAQUETE)
+})
+radio.onReceivedString(function (receivedString) {
+    Dirección_origen_recibida = receivedString.substr(0, 2)
+    Dirección_destino_recibida = receivedString.substr(2, 2)
+    Mensaje_recibido = receivedString.substr(4, 30)
+    if (Dirección_destino_recibida == Dirección_destino) {
+        basic.showString(Mensaje_recibido)
+    }
+})
+let Mensaje_recibido = ""
+let Dirección_destino_recibida = ""
+let Dirección_origen_recibida = ""
+let PAQUETE = ""
+let Dirección_destino = ""
 let Dirección_origen = "AR"
-let Dirección_destino = "GP"
-let MENSAJE = "" + Dirección_origen + Dirección_destino + "TREMENDO CULÓN"
+Dirección_destino = "GP"
+let CABECERA = "" + Dirección_origen + Dirección_destino
+let MENSAJE = "tremendo culo"
+PAQUETE = "" + CABECERA + MENSAJE
 basic.forever(function () {
-    radio.setGroup(0)
+    radio.setGroup(22)
 })
