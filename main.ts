76d309c97395654772aa06e7f6c1d7e80093b92d
CandeClara <131951638+CandeClara@users.noreply.github.com>
2023-05-22 13:25:45 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("" + (paquete_ping))
-    paquete_ping = input.runningTime()
+    radio.sendString(paquete_pong)
+    Mensaje_recibido = input.runningTime()
 })
 radio.onReceivedString(function (receivedString) {
     Dirección_origen_recibida = receivedString.substr(0, 2)
     Dirección_destino_recibida = receivedString.substr(2, 2)
-    Mensaje_recibido = receivedString.substr(4, 30)
+    mensaje_enviado = receivedString.substr(4, 30)
     if (Dirección_destino_recibida == Dirección_destino) {
-        basic.showString(Mensaje_recibido)
+        basic.showString("" + (Mensaje_recibido))
     }
 })
-let Mensaje_recibido = ""
+let mensaje_enviado = ""
 let Dirección_destino_recibida = ""
 let Dirección_origen_recibida = ""
-let paquete_ping = 0
+let Mensaje_recibido = 0
+let paquete_pong = ""
 let Dirección_destino = ""
-let Dirección_origen = "AR"
-Dirección_destino = "GP"
+let Dirección_origen = "GP"
+Dirección_destino = "AR"
 let CABECERA = "" + Dirección_origen + Dirección_destino
-let MENSAJE = "ping"
-paquete_ping = "" + CABECERA + MENSAJE
+let MENSAJE = paquete_pong
+paquete_pong = "" + CABECERA + MENSAJE
 basic.forever(function () {
     radio.setGroup(22)
 })
